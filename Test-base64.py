dbb2950232027a0888d71fe4455ec792a1c1e7f9
Ricardo <ribare12@gmail.com>
2020-09-12 22:48:13 -0500

    Obtencion de Sello

--- Test-base64.py
+++ Test-base64.py
@@ -1,30 +1,37 @@
 import base64
 import lxml.etree as ET
 import os
+from M2Crypto import RSA
+import hashlib
 
 PATH = os.path.abspath(os.path.dirname(__file__))
 
+# ->> Certificado a Base64
 '''cert_file = open(PATH + "\\certKey\\CertificadoFirmadoPF.cer","rb")  # r = read, b = binario
 cert = base64.b64encode(cert_file.read())
 certificado = cert.decode('utf-8')
 print (certificado)'''
 
-
-'''xml_file = open(PATH + "\\XML_Ejemplo_33.xml","r")
+# ->> Cadena Original
+xml_file = open(PATH + "/XML_Ejemplo_33.xml","r")
 dom = ET.parse(xml_file)
-xslt = ET.parse(PATH + "\\xslt\\cadenaoriginal_3_3.xslt")
+xslt = ET.parse(PATH + "/xslt/cadenaoriginal_3_3.xslt")
 transform = ET.XSLT(xslt)
 cadena_original = transform(dom)
 #print(ET.tostring(newxml, pretty_print = True))
-print(str(cadena_original))'''
+#print(str(cadena_original))
 
+# ->> Sello
+key = RSA.load_key(PATH + "/certKey/key.pem")
+digest = hashlib.new('sha256', str(cadena_original).encode('utf-8')).digest()
+sello = base64.b64encode(key.sign(digest,"sha256"))
+print(sello.decode('utf-8'))
 
-#key = RSA.load_key(PATH + "\\certKey\\key.pem")
-
-
-'''xml_file = open(PATH + "\\XML_Ejemplo_33.xml","rb").read()
+# Para leer xml y pasarlo a String
+'''xml_file = open(PATH + "/XML_Ejemplo_33.xml","rb").read()
 #dom = ET.fromstring(xml_file)
 print(xml_file.decode('utf-8'))'''
+
 
 # Obtener Path
 #print(os.path.abspath(os.getcwd()))
